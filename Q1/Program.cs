fcf3b92fdfac38fb78e636faa5473bdd160305b3
士允 王 <terryswang0729@gmail.com>
2022-11-09 22:02:57 +0800

    1109 v1 finish

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -10,10 +10,25 @@ namespace Q1
 	{
 		static void Main(string[] args)
 		{
+			List<int> list = new List<int> { 1,34,44,56,66,79 };
+			Func<int ,bool> isEven=n=>n%2==0;
+			List<int> evenItems=GetEvenItems(list, isEven);
+		    foreach(int answer in evenItems)
+			{
+				Console.WriteLine(answer);
+			}
 		}
-		 static List<int> GetEvenItems(List<int> source,Func<int,bool>)
+		 static List<int> GetEvenItems(List<int> source,Func<int,bool>funv)
 		{
-
+			var result=new List<int>();
+			foreach(int item in source)
+			{
+				if(funv(item)==true)
+				{
+					result.Add(item);
+				}
+			}
+			return result;
 		}
 	}
 }
