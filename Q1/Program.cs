a5ec48f6f36ec51ff6ec20984ede933b0ca93905
士允 王 <terryswang0729@gmail.com>
2022-11-09 17:23:35 +0800

    1109 v1 unfinish

---
+++ Q1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		 static List<int> GetEvenItems(List<int> source,Func<int,bool>)
+		{
+
+		}
+	}
+}
